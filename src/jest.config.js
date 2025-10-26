145c1ba7c7ab24ef90c21d69545b27df3baa1693
Bonarerijt <prudyjj4@gmail.com>
2025-10-26 19:49:41 +0300

    Updated Babel

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  transform: {
+    "^.+\\.[jt]sx?$": "babel-jest",
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!(@adobe/css-tools)/)", // ✅ Allow transpiling of modern modules
+  ],
 };
